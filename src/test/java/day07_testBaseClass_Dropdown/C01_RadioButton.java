1d7de1f1fd756d4356267b736d486cf741a9a276
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2023-11-24 11:37:26 +0300

    day 8

--- src/test/java/day07_testBaseClass_Dropdown/C01_RadioButton.java
+++ src/test/java/day07_testBaseClass_Dropdown/C01_RadioButton.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -46,6 +47,7 @@ public class C01_RadioButton {
 
         erkekRadioButton.click();
 
+        erkekRadioButton.sendKeys(Keys.PAGE_DOWN);
         //	c. Sectiginiz radio button’un seçili, ötekilerin seçili olmadigini test edin
 
         Assert.assertTrue(erkekRadioButton.isSelected());
