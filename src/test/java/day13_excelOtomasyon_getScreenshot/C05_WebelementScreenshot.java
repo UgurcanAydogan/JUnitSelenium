65402e1c2156de13c3e1cae93220bef4155cf88e
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2023-12-04 13:32:04 +0300

    day 13

--- src/test/java/day13_excelOtomasyon_getScreenshot/C05_WebelementScreenshot.java
+++ src/test/java/day13_excelOtomasyon_getScreenshot/C05_WebelementScreenshot.java
@@ -1,18 +1,22 @@
 package day13_excelOtomasyon_getScreenshot;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import utilities.ReusableMethods;
 import utilities.TestBase;
 
-public class C04_ScreenshotIleAramaTesti extends TestBase {
+import java.io.File;
+import java.io.IOException;
+
+public class C05_WebelementScreenshot extends TestBase {
 
     @Test
-    public void aramaTesti(){
-
+    public void aramaTesti() throws IOException {
         // testotomasyonu anasayfaya gidin
         driver.get("https://www.testotomasyonu.com");
 
@@ -28,9 +32,22 @@ public class C04_ScreenshotIleAramaTesti extends TestBase {
         Assert.assertNotEquals(unExpectedAramaSonucu,actualAramaSonucu);
 
         ReusableMethods.bekle(3);
+        // arama sonuc yazisinin screenshot'ini kaydedin
 
-        // tum sayfanin screenshot'ini kaydedin
+        // 1.adim screenshot alacagimiz webelementi locate et
 
-        ReusableMethods.tumSayfaTakeScreenshot("aramaTesti",driver);
+        // 2.adim scrennshot'i kaydedecegimiz file'i olusturalim
+
+        File istenenWebelementScreenshot = new File("target/screenshots/istenenWebelementScreenshot.jpg");
+
+        // 3.adim webelement uzerinden screenshot'i alip gecici bir dosyaya kaydedin
+
+        File geciciDosya = aramaSonucElementi.getScreenshotAs(OutputType.FILE);
+
+        // 4.adim gecici dosyayi asil dosyaya kopyalayalim
+
+        FileUtils.copyFile(geciciDosya,istenenWebelementScreenshot);
+
+
     }
 }
